103ebd6179a0c81aabed7f7534c67baa291f97ef
Gustavo da Hora <gustavo.carmodahora@outlook.com>
2022-05-23 17:21:58 -0300

    Adding new function to open and close edit text and save items.

--- app/src/main/java/dev/gustavodahora/todos/view/MainActivity.kt
+++ app/src/main/java/dev/gustavodahora/todos/view/MainActivity.kt
@@ -1,7 +1,11 @@
 package dev.gustavodahora.todos.view
 
 import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
+import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import dev.gustavodahora.todos.databinding.ActivityMainBinding
 
@@ -18,8 +22,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun listeners() {
+        // Start edit text
         mBinding.cntEditText.setOnClickListener {
             showEditTextAndKeyboard()
+        }
+        // When the keyboard enter button is pressed.
+        mBinding.editText.setOnEditorActionListener { view, actionId, event ->
+            if (actionId != 0 || event?.action == KeyEvent.ACTION_DOWN) {
+                // Action
+                Log.i("event", "finish add todo")
+                view.text = ""
+                false
+            } else {
+                true
+            }
         }
     }
 
